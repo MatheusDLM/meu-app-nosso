718539248557db510c6700b62dc1fc2d6686d825
Matheus Maia <matheus.maia100@hotmail.com>
2019-03-15 21:36:56 -0300

    2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
-  declarations: [AppComponent, HelloWorldComponent],
+  declarations: [AppComponent, HelloWorldComponent, CardComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
